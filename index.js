21232cc61fbecfb425c4d05feafd0e03f7d08852
kairimdb <kairimdb@gmail.com>
2024-03-14 15:33:26 +0800

    updated functions

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 console.log("hiwooo");
 
-function generateRandomNumber() {
-    let randomNum = Math.floor(Math.random() * 15) + 1
+generateRandomNumber(15);
+function generateRandomNumber(max) {
+    let randomNum = Math.floor(Math.random() * max) + 1;
 
 
     // if...else if...else statement
     // if (condition) {block of code to execute}
     // =, ==, !=, >, <, > =, < =, &&, ||
 
-    if(randomNum >= 15){
-        console.log(`Random Number is >= to 15`)
+    if (randomNum <= 15) {
+        return console.log(`Random Number ${randomNum} is in B. 1-15`);
+
+    } else if (randomNum >= 16 && randomNum <= 30) {
+        return console.log(`Random Number ${randomNum} is in I. 16-30`);
+
     } else {
-        console.log(`Random Number is <= to 15`)
+        return console.log(`Random Number is invalid`);
     }
 
+    // switch(randomNum){
+    // case 1:
+    //     console.log(`Random Number is in B.`)
+    //     break;
 
-    return console.log(randomNum);
+
+    //  case 16:
+    //     console.log(`Random Number is in I.`)
+    //     break;
+
+
+    //  case 31:
+    //     console.log(`Random Number is in N.`)
+    //     break;
+
+    // case 46:
+    //     console.log(`Random Number is in G.`)
+    //     break;
+
+
+    // case 61:
+    //     console.log(`Random Number is in O.`)
+    //     break;
 
 }
+//}
