33cb81d5e57f11436f9a36b2037c5fd5908e8f40
kairimdb <kairimdb@gmail.com>
2024-03-14 15:37:51 +0800

    bingo completed

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ function generateRandomNumber(max) {
     } else if (randomNum >= 16 && randomNum <= 30) {
         return console.log(`Random Number ${randomNum} is in I. 16-30`);
 
+    } else if (randomNum >= 31 && randomNum <= 45) {
+        return console.log(`Random Number ${randomNum} is in N. 31-45`);
+
+    } else if (randomNum >= 46 && randomNum <= 60) {
+        return console.log(`Random Number ${randomNum} is in G. 31-45`);
+
+    } else if (randomNum >= 61 && randomNum <= 75) {
+        return console.log(`Random Number ${randomNum} is in O. 31-45`);
+
     } else {
         return console.log(`Random Number is invalid`);
     }
